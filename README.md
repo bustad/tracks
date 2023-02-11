70075aadff530f4fc954b0d7f1f51b9be2f29ac2
Christofer Bustad <tosse_bustad@hotmail.com>
2023-02-11 15:26:11 +0100

    Few things.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Downloader and playlist creator of songs from Tracks
 
-See `tracks.ipynb` for an overview of the entire process.
+Some early experiments are found in `tracks.ipynb`.
 
 [Launch notebook on binder.org.](https://mybinder.org/v2/gh/bustad/tracks/main)
+
+1. Run `pyhton3 tracks-downloader.py` to download all the entries into `tracks-db.txt`.
+2. Run `python3 tracks-db-remove-duplicates.py` to remove duplicates and save the remaining entries to `tracks-db-no-duplicates.txt`.
--- tracks-downloader.py
+++ tracks-downloader.py
@@ -7,47 +7,38 @@ import html
 import datetime
 import time
 
-
 def g(instring):
-  if len(instring) == 1:
-    return " " + instring
-  else:
-    return instring
-
+    if len(instring) == 1:
+        return " " + instring
+    else:
+        return instring
 
 def downloaddate(date):
-  website = "https://sverigesradio.se/topplista.aspx?programid=2696&date="
+    website = "https://sverigesradio.se/topplista.aspx?programid=2696&date="
 
-  try:
-    with urllib.request.urlopen(website + date) as fp:
-      mybytes = fp.read()
-  except urllib.error.HTTPError as e:
-    #print('Error code: ', e.code)
-    return ""
+    try:
+        with urllib.request.urlopen(website + date) as fp:
+            mybytes = fp.read()
+    except urllib.error.HTTPError as e:
+        return ""
 
-  #with urllib.request.urlopen(website) as fp:
-  #  mybytes = fp.read()
+    mystr = mybytes.decode("utf8")
+    mystr = html.unescape(mystr)
+    mystr = mystr.split("\r\n")
+    mystr = [l.strip() for l in mystr]
 
-  mystr = mybytes.decode("utf8")
-  mystr = html.unescape(mystr)
-  mystr = mystr.split("\r\n")
-  mystr = [l.strip() for l in mystr]
+    result = ""
+    for k in range(len(mystr)):
+        if mystr[k] == '<li class="track">':
+            result += date + ", "
+            if mystr[k + 3] == '<div class="track__ranking-current">':
+                result += g(mystr[k + 4].split()[0]) + ", "
+                result += mystr[k + 8][len('<span class="track__title">'):-len('</span>')]
+                result += "\n"
+            else:
+                result += " B, " + mystr[k + 3][len('<span class="track-title">'):-len('</span>')] + "\n"
 
-  result = ""
-  for k in range(len(mystr)):
-    if mystr[k] == '<li class="track">':
-      result += date + ", "
-      if mystr[k + 3] == '<div class="track__ranking-current">':
-        result += g(mystr[k + 4].split()[0]) + ", "
-        result += mystr[k +
-                        8][len('<span class="track__title">'):-len('</span>')]
-        result += "\n"
-      else:
-        result += " B, " + mystr[
-          k + 3][len('<span class="track-title">'):-len('</span>')] + "\n"
-
-  return result
-
+    return result
 
 start_date = datetime.date(1984, 1, 1)
 end_date = datetime.date(2011, 1, 1)
@@ -55,9 +46,8 @@ delta = datetime.timedelta(days=1)
 current_date = start_date
 
 while (current_date < end_date):
-  print(current_date, end="\n")
-  with open("tracks-db.txt", "a") as myfile:
-    myfile.write(downloaddate(str(current_date)))
-  time.sleep(0.5)
-  current_date += delta
-
+    print(current_date, end="\n")
+    with open("tracks-db.txt", "a") as myfile:
+        myfile.write(downloaddate(str(current_date)))
+    time.sleep(0.5)
+    current_date += delta
