b6232612d3385fb5bb8c7eb6f6edf9adfd57a9bd
Christofer Bustad <tosse_bustad@hotmail.com>
2023-02-12 17:49:44 +0100

    Few things.

--- generate_playlist.py
+++ generate_playlist.py
@@ -10,10 +10,6 @@ args = parser.parse_args()
 
 filename_in = "tracks-db-no-duplicates.txt"
 
-###############################################################################
-# Create a new playlist...
-###############################################################################
-
 token_headers = {
     "Content-Type": "application/json",
     "Authorization": "Bearer " + args.token,
@@ -27,13 +23,6 @@ token_data = {
 r = requests.post("https://api.spotify.com/v1/users/" + args.user_id + "/playlists", data=json.dumps(token_data), headers=token_headers)
 playlist_id = r.json()["id"]
 
-#for key, value in token_etc.items():
-#    print(key, ':\n  ', value)
-
-###############################################################################
-# Go through each track from that year...
-###############################################################################
-
 nr_added = 0
 
 with open(filename_in) as file:
@@ -42,9 +31,6 @@ with open(filename_in) as file:
             track = line.strip()[16:].split(" - ")
             artist = track[0]
             song = track[1]
-            #print(artist, "-", song)
-
-            # Search for artist/song...
             
             user_headers = {
                 "Authorization": "Bearer " + args.token,
@@ -54,19 +40,14 @@ with open(filename_in) as file:
 
             user_params = {
                 "limit": 10,
-                #"q": "artist:MO-DO track:EINS, ZWEI, POLIZEI",
                 "q": "artist:" + artist + " track:" + song,
                 "type": "track"
             }
 
-            #print("artist:" + artist + " track:" + song)
-
             r = requests.get("https://api.spotify.com/v1/search", params=user_params, headers=user_headers).json()
-            #print(r['tracks'].keys())
+
             if len(r['tracks']['items']) > 0:
                 track_uri = r['tracks']['items'][0]['uri']
-
-                # Add track_uri to playlist_id
 
                 token_headers = {
                     "Content-Type": "application/json",
@@ -76,10 +57,6 @@ with open(filename_in) as file:
                 token_data = {"uris": [track_uri]}
 
                 r = requests.post("https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks", data=json.dumps(token_data), headers=token_headers)
-                #token_etc = r.json()
-
-                #for key, value in token_etc.items():
-                #    print(key, ':\n  ', value)
 
                 nr_added += 1
             else:
